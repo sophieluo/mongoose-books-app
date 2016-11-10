0ebf4f2148efe5f4a3029a0ec3acb86f242e8956
sophieluo <sophie@groobusiness.com>
2016-11-10 11:11:02 -0800

    sprint 2

--- models/index.js
+++ models/index.js
@@ -1,4 +1,6 @@
 var mongoose = require("mongoose");
+
 mongoose.connect("mongodb://localhost/book-app");
 
 module.exports.Book = require("./book.js");
+module.exports.Author = require("./author.js");
